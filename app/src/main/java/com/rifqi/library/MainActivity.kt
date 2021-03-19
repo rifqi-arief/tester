338ee92ad597262bec816cd5ce7f4c51bd5b63c4
Sumringah <rifkiarif46@gmail.com>
2021-03-19 13:13:00 +0700

    aktif proguuard

--- app/src/main/java/com/rifqi/library/MainActivity.kt
+++ app/src/main/java/com/rifqi/library/MainActivity.kt
@@ -2,11 +2,16 @@ package com.rifqi.library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import id.zelory.cekrek.Cekrek
+import android.util.Log
+import id.qiqi.test.Math
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val math = Math()
+        var hasil = math.Penjumlahan(1,1)
+        Log.i("tester", hasil.toString())
     }
 }
